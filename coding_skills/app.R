b0ae93f486f6e78dcd6ea066971bdef163ea346c
ghappy112 <65770357+ghappy112@users.noreply.github.com>
2024-07-08 13:55:55 -0500

    Update app.R
    added cloud and software skills to coding skills dashboard

--- coding_skills/app.R
+++ coding_skills/app.R
@@ -7,7 +7,7 @@ ui <- page_sidebar(
   sidebar = sidebar(
     sidebarPanel(
       checkboxGroupInput("filter", "Filter:",
-                         choices = c("Coding Languages", "Python Packages", "R Packages"),
+                         choices = c("Cloud", "Coding Languages", "Python Packages", "R Packages", "Software"),
                          selected = c("Coding Languages")),
       width = 100
     )),
@@ -22,9 +22,9 @@ server <- function(input, output) {
   
   # Define dataframe with categories and frequencies
   df = data.frame(
-    Language = c("Python", "R", "Stata", "SQL", "HTML", "CSS", "JavaScript", "DAX", "Pandas", "NumPy", "SciPy", "scikit-learn", "Keras", "TensorFlow", "Matplotlib", "Seaborn", "Regex", "requests", "Beautiful Soup", "Selenium", "json", "XGBoost", "CATBoost", "pickle", "joblib", "statsmodels", "NLTK", "Flask", "Shiny", "Stargazer", "ggplot2", "plotly", "tidy2", "dplyr", "PySpark", "Transformers"),
-    Skill = c(2.5, 2, 1.5, 3, 1, 0.5, 1, 2, 3, 2, 1.5, 3, 2, 2, 2, 2, 3, 2, 2.5, 1, 1.5, 2, 2, 1, 1, 1, 2, 1, 2, 2, 2, 1, 1, 1, 1, 1.5),
-    filter = c("Coding Languages", "Coding Languages", "Coding Languages", "Coding Languages", "Coding Languages", "Coding Languages", "Coding Languages", "Coding Languages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "R Packages", "R Packages", "R Packages", "R Packages", "R Packages", "R Packages", "Python Packages", "Python Packages")
+    Language = c("Python", "R", "Stata", "SQL", "HTML", "CSS", "JavaScript", "DAX", "Pandas", "NumPy", "SciPy", "scikit-learn", "Keras", "TensorFlow", "Matplotlib", "Seaborn", "Regex", "requests", "Beautiful Soup", "Selenium", "json", "XGBoost", "CATBoost", "pickle", "joblib", "statsmodels", "NLTK", "Flask", "Shiny", "Stargazer", "ggplot2", "plotly", "tidy2", "dplyr", "PySpark", "Transformers", "Power BI", "Tableau", "Excel", "Power Automate", "Power Apps", "AWS", "Azure"),
+    Skill = c(2.5, 2, 1.5, 3, 1, 0.5, 1, 2, 3, 2, 1.5, 3, 2, 2, 2, 2, 3, 2, 2.5, 1, 1.5, 2, 2, 1, 1, 1, 2, 1, 2, 2, 2, 1, 1, 1, 1, 1.5, 2, 1, 2, 1, 1, 2, 0.5),
+    filter = c("Coding Languages", "Coding Languages", "Coding Languages", "Coding Languages", "Coding Languages", "Coding Languages", "Coding Languages", "Coding Languages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "Python Packages", "R Packages", "R Packages", "R Packages", "R Packages", "R Packages", "R Packages", "Python Packages", "Python Packages", "Software", "Software", "Software", "Software", "Software", "Cloud", "Cloud")
   )
   # engineer sorting order feature for dataframe for sorting bar chart
   df = df[order(df$Language), ]
